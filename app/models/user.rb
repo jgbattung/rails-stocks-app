2d0e7aaa59e858d811ad663ec409d6b8a9b91340
Jireh Battung <jirehgbattung@gmail.com>
2022-09-04 19:08:40 +0800

    added devise to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :user_roles
     has_many :roles, through: :user_roles
 end
