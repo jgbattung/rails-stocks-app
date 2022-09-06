e47db646d3195f0ca25d617ee2ef52c4ed436f0f
Jireh Battung <jirehgbattung@gmail.com>
2022-09-06 19:04:16 +0800

    added after signout path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(user)
         dashboard_path
     end
+
+    def after_sign_out_path_for(user)
+        root_path
+    end
+    
 end
