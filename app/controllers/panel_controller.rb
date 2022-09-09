22263598660708933887a3f1c0e8f383ac2cb4d0
Jireh Battung <jirehgbattung@gmail.com>
2022-09-09 15:49:57 +0800

    not allow trader to access admin panel

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,6 +1,12 @@
 class PanelController < ApplicationController
     before_action :authenticate_user!
+    before_action :correct_user
   
     def index
+    end
+
+    def correct_user
+      @user = current_user
+      redirect_to dashboard_path, notice: 'You are not authorized to view this page' unless @user.role == 'admin'
     end
   end
