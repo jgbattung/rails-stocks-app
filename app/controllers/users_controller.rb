e3e89d6c2bd24339ef2ece42c2b6835fe99096bd
Jireh Battung <jirehgbattung@gmail.com>
2022-09-12 18:59:12 +0800

    added commands to def edit in user controller to fix approval by admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,11 @@ class UsersController < ApplicationController
     end
 
     def new
-        @user = User.new
+      @user = User.new
     end
 
     def edit
+      @user = User.find(params[:id])
     end
 
     def create
